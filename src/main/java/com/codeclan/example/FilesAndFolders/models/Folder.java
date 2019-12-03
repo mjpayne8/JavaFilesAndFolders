6281994e71fe81af22469690e450aee35dc4c568
Mark Payne <mjpayne8@gmail.com>
2019-12-03 16:06:13 +0000

    class Folder created and annotated

--- src/main/java/com/codeclan/example/FilesAndFolders/models/Folder.java
+++ src/main/java/com/codeclan/example/FilesAndFolders/models/Folder.java
@@ -1,4 +1,65 @@
 package com.codeclan.example.FilesAndFolders.models;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Folder {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "files")
+    private List<File> files;
+
+    @Column(name = "user")
+    private User user;
+
+    public Folder(String title, User user) {
+        this.title = title;
+        this.user = user;
+        this.files = new ArrayList<File>();
+    }
+
+    public Folder(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<File> getFiles() {
+        return files;
+    }
+
+    public void setFiles(List<File> files) {
+        this.files = files;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
